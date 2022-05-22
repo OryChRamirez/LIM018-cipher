325e11c39c4ba69b06fbee27409da235f8e879e7
OryChRamirez <ory.maiden@gmail.com>
2022-05-21 20:07:21 -0500

    separación de módulos

--- src/index.js
+++ src/index.js
@@ -1,3 +1,36 @@
 import cipher from './cipher.js';
 
-console.log(cipher);
+// console.log(cipher); 
+
+let btnCifrador = document.getElementById('btnCifrador');
+btnCifrador.addEventListener('click',pantallaCifrador);
+
+let btnInfo = document.getElementById('btnInfo');
+btnInfo.addEventListener('click',pantallaInfo);
+
+function pantallaCifrador(){
+    document.getElementById('inicio').style.display = 'none';
+    document.getElementById('informacion').style.display = 'none';
+    document.getElementById('cifrador').style.display = 'block';
+}
+
+function pantallaInfo(){
+    document.getElementById('inicio').style.display = 'none';
+    document.getElementById('cifrador').style.display = 'none';
+    document.getElementById('informacion').style.display = 'block';
+    document.getElementById("body").style.backgroundImage = 'none';
+}
+
+let mayusculas = document.getElementById('mensaje');
+mayusculas.addEventListener('keyup', function(){
+  this.value = this.value.toUpperCase();
+})
+
+let btnCifrar = document.getElementById('btnCifrar');
+btnCifrar.addEventListener('click',btnCifrado);
+
+function btnCifrado(){
+    let msjIngresado = document.getElementById('mensaje').value;
+    let desplazamiento = document.getElementById('desplazamiento').value;
+    document.getElementById('cifradoDescifrado').value = cipher.encode(msjIngresado, desplazamiento);
+  }
