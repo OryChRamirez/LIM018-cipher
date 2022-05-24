39381b064d5825f3293dffa2bf157f9a59b697e2
OryChRamirez <ory.maiden@gmail.com>
2022-05-24 00:22:11 -0500

    Cambie las formulas aplicando el codigo ASCII y usando el caso para mayus y minus

--- src/cipher.js
+++ src/cipher.js
@@ -1,39 +1,50 @@
 const cipher = {
-  encode : function(msjIngresado, desplazamiento){
-    let msjCifrado = "";
-    let alfabeto = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
-    desplazamiento = (desplazamiento % 26 + 26) % 26;
-  
-    if(msjIngresado){
-  
-      for(let i=0; i<msjIngresado.length; i++){
-        if(alfabeto.indexOf(msjIngresado[i])!=-1){
-          let posicion = ((alfabeto.indexOf(msjIngresado[i])+desplazamiento)%26);
-          msjCifrado += alfabeto[posicion];
-        }
-        else 
-        msjCifrado += msjIngresado[i];
-      }
-    }
-    return msjCifrado;
+  encode : function(string, offset){
+      let codeMayus ="";
+      let codeMinus ="";
+      let codificador="";
+      let codificador2="";
+
+      offset = parseInt(offset);
+    if(string === string.toUpperCase()){
+      for(let i=0;i<=string.length;i++){
+        codificador = (string.charCodeAt(i) - 65 + offset)%26 + 65;
+        if(codificador < 65){
+       codificador =(string.charCodeAt(i) - 65 + offset)%26 + 65 + 26;
+        codeMayus = codeMayus + String.fromCharCode(codificador);
+        }else{
+        codeMayus = codeMayus + String.fromCharCode(codificador);
+    }}
+    return codeMayus;
+  }else if(string === string.toLowerCase()){
+    for(let i=0;i<=string.length;i++){
+      codificador2 = (string.charCodeAt(i) - 97 + offset)%26 + 97;
+      if(codificador2 < 65){
+      codificador2 = (string.charCodeAt(i) - 97 + offset)%26 + 97 +26;
+        codeMinus = codeMinus + String.fromCharCode(codificador2);
+      }else{
+        codeMinus = codeMinus + String.fromCharCode(codificador2);      
+    }}
+    return codeMinus;
+  }},
+  decode : function (string,offset){
+    let decodeMayus ="";
+      // let decodeMinus ="";
+      let decodificador="";
+      // let decodificador2="";
+      offset = parseInt(offset);
+      if(string === string.toUpperCase()){
+        for(let i=0;i<=string.length;i++){
+          decodificador = (string.charCodeAt(i) - 65 - offset)%26 + 65;
+          if(decodificador < 65){
+            decodificador =(string.charCodeAt(i) - 65 - offset)%26 + 26 + 65;
+            decodeMayus = decodeMayus + String.fromCharCode(decodificador);
+                    }else{
+          decodeMayus = decodeMayus + String.fromCharCode(decodificador);
+      }}
+      return decodeMayus;
+    }      
   }
 
-};
-
+  }
 export default cipher;
-
-
-
-
-
-
-
-/* let btnDescifrar = document.getElementById('btnDescifrar');
-btnDescifrar.addEventListener('click',btnDescifrado); 
-
-function btnDescifrado(){
-  let msjIngresado = document.getElementById('mensaje').value;
-  let desplazamiento = document.getElementById('desplazamiento').value;
-  document.getElementById('cifradoDescifrado').value = msjIngresado;  //está arrojando el valor del mensaje ingresado (falta agregar la otra función)
-
-} */
